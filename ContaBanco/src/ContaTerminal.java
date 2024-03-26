ab3897d9e002b87a9d0bb8e3ce532d5152ee3d7a
gilsonbs <gilson.barreto2009@gmail.com>
2024-03-26 20:19:26 -0300

    ContaBanco primeiro desafio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int Numero;
+        String Agencia;
+        String NomeCliente;
+        Float Saldo;
+
+        //"Por Favor, digite o número da Agência!"
+        //"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponivel para saque".
+
+
+        
+    }
+}
