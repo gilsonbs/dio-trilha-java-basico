f4a319d2b7ba9c7effa702f5d0b61e5176825253
gilsonbs <gilson.barreto2009@gmail.com>
2024-03-26 21:18:00 -0300

    DESAFIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        int Numero;
-        String Agencia;
-        String NomeCliente;
-        Float Saldo;
+        Scanner scanner = new Scanner(System.in);
+                      
+        System.out.println("Por Favor, digite o número da Agencia");
+        String Agencia = scanner.nextLine();
+    
+        System.out.println("Por Favor, digite o número da Conta!");
+        int Numero = scanner.nextInt();
 
-        //"Por Favor, digite o número da Agência!"
-        //"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponivel para saque".
+        scanner.nextLine();
 
-
+        System.out.println("Por Favor, digite seu nome!");
+        String NomeCliente = scanner.nextLine();
         
+
+        System.out.println("Por Favor, digite seu sobrenome!");
+        String sobreNomeCliente = scanner.nextLine();
+
+        System.out.println("Por Favor, quanto voce deseja depositar?");
+        Float Saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + NomeCliente + " " + sobreNomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + " conta " + Numero + " e seu saldo R$ " + Saldo + " já está disponivel para saque!");
+    
     }
 }
